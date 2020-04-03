0db416058f64e62d59f4ca01b09d93bc41144d1c
dujianjun <v@jianwi.cn>
2020-04-03 23:51:59 +0800

    基本布局

---
+++ vue_project/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar bg-secondary text-light">
+        <div class="container align-items-center">
+            <div class="navbar-brand">
+                好玩的小工具站
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- vue_project/src/views/Home.vue
+++ vue_project/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home offset-md-3 col-md-6">
+    <div class="card m-1 mt-3 pt-2">
+      <div class="card-header-pills">
+        原创区域
+      </div>
+      <div class="card-body">
+
+      </div>
+    </div>
+    <div class="card m-1 mt-2 pt-2">
+      <div class="card-header-pills">
+        非原创
+      </div>
+      <div class="card-body">
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
