27f64bf161d1e11e9b6517132021a9c79c64afa3
dujianjun <v@jianwi.cn>
2020-04-04 20:59:18 +0800

    首页导航布局

--- vue_project/src/views/Home.vue
+++ vue_project/src/views/Home.vue
@@ -4,8 +4,12 @@
       <div class="card-header-pills">
         原创区域
       </div>
-      <div class="card-body">
+      <div class="card-body container">
+        <div class="row">
+          <router-link class="offset-1 col-5 btn btn-primary" to="/xuexitong">学习通倍速播放</router-link>
 
+          <router-link class="col-5 btn mx-2 btn-primary" to="">青年大学习截图</router-link>
+        </div>
       </div>
     </div>
     <div class="card m-1 mt-2 pt-2">
